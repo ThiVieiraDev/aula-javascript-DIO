a23c97bd91078861645068f11cf991869b054413
Thi Vieira <thiiagoviieira@gmail.com>
2021-01-02 02:02:17 -0300

    Array e Dicionário

---
+++ js/main.js
@@ -0,0 +1,14 @@
+ var idade = prompt("Qual a sua idade ?");
+ if (idade >= 18) alert("Pode acessar a página");
+ else alert("Não pode acessar a página");
+
+
+//  var lista = ["maça", "pêra", "laranja"];
+//  console.log(lista);
+//  alert(lista[2]);
+
+// var nome = "Thiago Vieira";
+// var idade = 22;
+// alert("Meu nome é " + nome + " e tenho " + idade + " anos");
+
+// console.log(nome + " " + idade);
